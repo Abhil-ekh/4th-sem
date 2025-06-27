5f203d83f476e2078daf12a48536dd5c8f9736cd
Abhil-ekh <165144949+Abhil-ekh@users.noreply.github.com>
2025-06-27 14:28:05 +0545

    Update lab2.cpp

--- TOC/lab2.cpp
+++ TOC/lab2.cpp
@@ -11,28 +11,41 @@ int main() {
     int state=0;
     for(char c:input) {
     	if (state == 0) {
-            if (c == '1') 
+            if (c == 'a') 
 				state = 1;
             else 
 				state = -1;  
         }
         else if (state == 1) {
-            if (c == '0') 
-				state = 2;
-            else if (c == '1') 
+            if (c == 'a') 
 				state = 1;
+            else if (c == 'b') 
+				state = 2;
             else {
                 state = -1; 
                 break;
             }
         }
         else if (state == 2) {
-            if (c == '0') 
-				state = 3;
+            if (c == 'a') 
+				state = 1;
+			else if (c == 'b') 
+				state = 2;
             else {
                 state = -1;
                 break;
             }
         }
+        else if (state == 4) {
+            state = -1;
+            break;
+        }
 	}
+	if (state == 2) {
+        cout << "Accepted: The string is '1001'\n";
+    } else {
+        cout << "Rejected\n";
+    }
+
+    return 0;
 }
