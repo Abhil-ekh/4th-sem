e3975412afacab9d76878a7032d89c1c59f96c4d
Abhil-ekh <165144949+Abhil-ekh@users.noreply.github.com>
2025-06-27 14:22:45 +0545

    Add files via upload

---
+++ TOC/lab2.cpp
@@ -0,0 +1,38 @@
+//DFA THAT ACCEPTS STRING THAT STARTS WITH "a" AND ENDS WITH "b"
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	string input;
+	cout << "Enter a string over {0,1}: ";
+    cin >> input;
+    int state=0;
+    for(char c:input) {
+    	if (state == 0) {
+            if (c == '1') 
+				state = 1;
+            else 
+				state = -1;  
+        }
+        else if (state == 1) {
+            if (c == '0') 
+				state = 2;
+            else if (c == '1') 
+				state = 1;
+            else {
+                state = -1; 
+                break;
+            }
+        }
+        else if (state == 2) {
+            if (c == '0') 
+				state = 3;
+            else {
+                state = -1;
+                break;
+            }
+        }
+	}
+}
