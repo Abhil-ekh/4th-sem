c4a59f78516e62bf837fe61426a3c4050874b638
Abhil-ekh <165144949+Abhil-ekh@users.noreply.github.com>
2025-11-05 09:43:28 +0545

    Update lab12.cpp

--- OS/lab12.cpp
+++ OS/lab12.cpp
@@ -1,42 +1,41 @@
-#include <iostream> 
-#include <vector> 
-using namespace std; 
- 
-int main() { 
-    int nBlocks, nProcesses; 
-    cout << "Enter number of memory blocks: "; 
-    cin >> nBlocks; 
-    cout << "Enter number of processes: "; 
-    cin >> nProcesses; 
- 
-    vector<int> blockSize(nBlocks), processSize(nProcesses), allocation(nProcesses, -1); 
- 
-    cout << "Enter block sizes:\n"; 
-    for (int i = 0; i < nBlocks; i++) cin >> blockSize[i]; 
- 
-    cout << "Enter process sizes:\n"; 
-    for (int i = 0; i < nProcesses; i++) cin >> processSize[i]; 
- 
-    for (int i = 0; i < nProcesses; i++) { 
-        int worstIdx = -1; 
-        for (int j = 0; j < nBlocks; j++) { 
-            if (blockSize[j] >= processSize[i]) { 
-                if (worstIdx == -1 || blockSize[j] > blockSize[worstIdx]) 
-                    worstIdx = j; 
-            } 
-        } 
-        if (worstIdx != -1) { 
-            allocation[i] = worstIdx; 
-            blockSize[worstIdx] -= processSize[i]; 
-        } 
-    } 
- 
-    cout << "\nProcess No.\tProcess Size\tBlock No.\n"; 
-    for (int i = 0; i < nProcesses; i++) { 
-        cout << i+1 << "\t\t" << processSize[i] << "\t\t"; 
-        if (allocation[i] != -1) cout << allocation[i] + 1; 
-        else cout << "Not Allocated"; 
-        cout << "\n"; 
-    } 
-    return 0; 
-} 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, m;
+    cout << "Enter number of memory blocks: ";
+    cin >> n;
+    vector<int> block(n);
+    cout << "Enter sizes of blocks: ";
+    for (int i = 0; i < n; i++) cin >> block[i];
+
+    cout << "Enter number of processes: ";
+    cin >> m;
+    vector<int> process(m);
+    cout << "Enter sizes of processes: ";
+    for (int i = 0; i < m; i++) cin >> process[i];
+
+    vector<bool> allocated(n, false); // Track if a block is already used
+
+    cout << "\nWORST FIT Allocation:\n";
+    for (int i = 0; i < m; i++) {
+        int worstIdx = -1;
+
+        for (int j = 0; j < n; j++) {
+            if (!allocated[j] && block[j] >= process[i]) {
+                if (worstIdx == -1 || block[j] > block[worstIdx])
+                    worstIdx = j;
+            }
+        }
+
+        if (worstIdx != -1) {
+            cout << "Process " << i + 1 << " allocated to Block " << worstIdx + 1 << endl;
+            allocated[worstIdx] = true; // Mark as used
+        } else {
+            cout << "Process " << i + 1 << " not allocated\n";
+        }
+    }
+
+    return 0;
+}
